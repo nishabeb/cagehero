ca4f8ab49c2121e55d856b3aceed64967da00813
xzendercage <akramalikhan459@gmail.com>
2020-04-04 13:32:53 +0530

    initial commit

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @deadpool_army"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**Hoii Sir I'm Alive ψ(｀∇´)ψ`**\n\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xzendercage\n"
+                     "`Bot created by:` [NONN](tg://user?id=)\n"
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
+                     f"`My peru owner`: {DEFAULTUSER}\n"
+                     "[Deploy this userbot Now](https://github.com/xzendercage/UserAdnn)")
