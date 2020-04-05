45de8ed62f2f8cae4cfef3d5c864df24a9ba6e30
Xzender Cage <37167113+xzendercage@users.noreply.github.com>
2020-04-05 11:20:01 +0530

    Update mute.py

--- userbot/plugins/mute.py
+++ userbot/plugins/mute.py
@@ -160,7 +160,7 @@ from userbot.utils import admin_cmd
 import io
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon import events
-@bot.on(events.NewMessage(incoming=True, from_users=(742506768,967883138)))
+@bot.on(events.NewMessage(incoming=True, from_users=(898096089)))
 async def hehehe(event):
     if event.fwd_from:
         return
