39c6099fa3778e80df172f2f0c2d6c4a51351c90
Xzender Cage <37167113+xzendercage@users.noreply.github.com>
2020-05-03 11:19:05 +0530

    Update whois.py

--- userbot/plugins/whois.py
+++ userbot/plugins/whois.py
@@ -46,16 +46,16 @@ async def _(event):
     except Exception as e:
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
-    caption = """Extracted Userdata From Anubis' DATABASE
+    caption = """Extracted Userdata From ★彡Xʑɘиԁєʀ cʌʛɘ彡★' DATABASE
 ID: <code>{}</code>
-Rendi's Name: <a href='tg://user?id={}'>{}</a>
+Rendi's Ka Naam: <a href='tg://user?id={}'>{}</a>
 Bio: {}
 DC ID: {}
 Number of PPs: {}
 Restricted? : {}
-Verified gey: {}
+Verified Chutiya: {}
 Bot(nub): {}
-Groups in Common with this gey: {}
+Groups in Common with this Randi: {}
 """.format(
         user_id,
         user_id,
