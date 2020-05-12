cff76a4a66bebe87e329b2b012cc2988251f6770
Xzender Cage <37167113+xzendercage@users.noreply.github.com>
2020-05-12 13:22:46 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,13 +6,13 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @deadpool_army"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [AI BY](tg://user?id=898096089)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("ღღAt Your Service Sir i'm Aliveღღ\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @xzendercage\n"
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nFork by:` @xzendercage\n"
                      "`Bot created by:` ★彡Xʑɘиԁєʀ cʌʛɘ彡★ \n"
                      "`Database Status: Databases functioning Normally!\n\nAlways with you, My Master!\n`"
                      f"`My peru owner`: ★彡Xʑɘиԁєʀ cʌʛɘ彡★ \n"
